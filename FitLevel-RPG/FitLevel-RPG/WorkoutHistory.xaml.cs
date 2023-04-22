655a73ab49393a86784ba71b067f8f0d5198be5a
Marcus Ngooi <mngooi@gmail.com>
2023-04-21 21:26:33 -0400

    [MOD] Code cleaning

--- FitLevel-RPG/FitLevel-RPG/WorkoutHistory.xaml.cs
+++ FitLevel-RPG/FitLevel-RPG/WorkoutHistory.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,38 @@ namespace FitLevel_RPG
         public WorkoutHistory()
         {
             InitializeComponent();
+
+            //SqlConnection sqlCon = new(@"Data Source=fitlevelrpg1.database.windows.net;Initial Catalog=FitLevelRPG;User ID=rpglogin;Password=HiQ!w2g6SFS;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            //try
+            //{
+            //    if (sqlCon.State == System.Data.ConnectionState.Closed)
+            //    {
+            //        sqlCon.Open();
+            //        String getUser = "SELECT * FROM [User] WHERE user_id = @user_id";
+            //        SqlCommand cmd = new(getUser, sqlCon)
+            //        {
+            //            CommandType = System.Data.CommandType.Text
+            //        };
+            //        cmd.Parameters.AddWithValue("@user_id", LoggedInView.LoggedInUserID);
+
+            //        using SqlDataReader reader = cmd.ExecuteReader();
+            //        if (reader.Read())
+            //        {
+            //            usernameTextBox.Text = reader["username"].ToString();
+            //            emailTextbox.Text = reader["email"].ToString();
+            //            fullnameTextBox.Text = reader["full_name"].ToString();
+            //            dobTextbox.Text = reader["dob"].ToString();
+            //        }
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //}
+            //finally
+            //{
+            //    sqlCon.Close();
+            //}
         }
 
         private void BtnClickPlanNextWorkout(object sender, RoutedEventArgs e)
